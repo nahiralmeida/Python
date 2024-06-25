2519b3546ea714a5f5951483c5526ed28aa09b93
Nahir Almeida <nahiralmieda9@gmail.com>
2024-06-25 17:47:02 -0300

    primer subida a github del curso

---
+++ 18dejunio.py
@@ -0,0 +1,47 @@
+mi_variable=1994
+print(mi_variable)
+#### EL NOMBRE DE UAN VARIABLE SIEMPRE DEBE EMPEZAR POR UNA LETRA O POR UN GUIN BAJO (_)
+# LOS NOMBRE EN LAS VARIABLES NO PUEDEN INCLUIR ESPACIOS EN BLANCO 
+##PYTHON RECONOCE LAS MAYUSCULAS Y MINUSCULAS 
+
+mi_fecha_de_nacimiento = "01 de marzo"
+
+
+print(mi_fecha_de_nacimiento)
+
+##METODO DE SALIDA = PRINT ()
+
+##METODO DE ENTRADA = INPUT ()
+
+nombre = input("hola! escribi tu nombre:")
+
+print(nombre)
+
+## LA FUNCION INPUT POR DEFECTO CONVIERTE LOS DATOS EN ENTRADA EN UN STRING (ES UNA CADENA) AUNQUE LE ESTEMOS ESCRIBIENDO UN NUMERO 
+
+a = 20
+b = 30
+
+resultado = a+b
+
+print(resultado)
+
+c = 100
+d = 200
+
+print(c+d)
+
+# LOS DATOS DE ENTRADA SE PODRIAN CONVERTIR EN STR (STRING)
+
+e = 30
+"""f = input("cual es tu edad?") """#este es un ejemplo de entrada que lo toma como cadena de texto 
+f = int(input(cual es tu edad?))
+              
+print(e+f)
+
+cadena_de_texto = "python"
+anio_del_curso = "2024"
+
+print(cadena_de_texto + anio_del_curso)
+
+
---
+++ 4dejunio.py
@@ -0,0 +1,7 @@
+print(1-4)
+""" print(7+7) """
+
+#se puede comentar el codigo con numeral delante de una sola linea 
+"""o podes utilizar las triples comillas para comentar multilinea
+como este ejemplo  """
+
