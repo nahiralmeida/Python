c820c80cde74ed2e1b7c4b2b218c0f26c5f18865
Nahir Almeida <nahiralmieda9@gmail.com>
2024-07-02 19:24:53 -0300

    02dejulio.py

---
+++ 02dejulio.py
@@ -0,0 +1,63 @@
+#___ OPERADORES 
+
+#OPERANDO [operador] operando
+#   -/*+
+#operadores aritmeticos (+,-,*,/)
+# expresiones aritmeticas : 2+2, 5-1 , -1*100 , 20/3
+#expresiones algebraicas : radio * 3,14 (nota1+ nota2)/2
+
+#tipo logico : BOOLEANO o binario 
+# es el tipo de dato mas basico de la informacion racional y representa VERDADERO Y FALSO 
+
+#no verdadero = falso (TRUE)
+#no falso = verdadero (FALSE)
+
+
+#operadores relacionales : son simbolos que se usan para comparar los valores . si el resultado de la comparacion es correcto , la expresion va a ser considerada TRUE , de lo contrario sera False 
+
+print(1+1 == 3)
+
+# IGUALDAD 
+# EL oprerador de igualdad sirve para preguntarle al programa si ambos operandos son iguales . 
+#nos va a devolver TRUE si son iguales , y False si son distintos 
+#ESTE OPERADOR SE ESCRIBE CON DOS SIGNOS IGUAL (==)
+
+#ejemplo 
+
+a = 3
+print(a == 3)
+
+print (a !=3)
+
+#desigualdad o distinto 
+#el operador distinto sirve para preguntarle a nuestro programa si ambos operandos son distintos 
+#va a devolver TRUE si son distintos , False si son iguales 
+#este operador se escribe con un signo de exclamacion y un signo de igual (!=)
+
+#menor que 
+#sirve para preguntar a nuestro programa si el primer operando es menor que el segundo operando 
+# si el primero es menor TRUE , si el primero es mayor FALSE 
+# 7 < 3 
+print (7<3)
+
+#menor igual que 
+# el operador MENOR IGUAL QUE sirve para preguntar si el primer operando es menor que el segundo operando o si ambos son iguales 
+# TRUE  si el primero es menor o igual al segundo , y FALSE si el primero es mayor al segundo 
+# este operador se escribe con un signo menor que y un igual (<=)
+
+print(7<=3)
+print(10<=10)
+
+#MAYOR QUE 
+#el operador MAYOR IGUAL QUE sirve para preguntar si el primer operando es mayor que el segundo operando 
+# TRUE si el primero es mayor al segundo , y FALSE si el primero es menor al segundo 
+#(>)
+
+print(7>6)
+print(1>2)
+print(7>=3)
+print(10>=10)
+
+
+
+
