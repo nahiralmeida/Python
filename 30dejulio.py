2395e809a229f049c4a3772b1e6024d7bf6d9bef
Nahir Almeida <nahiralmieda9@gmail.com>
2024-07-30 19:00:52 -0300

    30dejulio.py

---
+++ 30dejulio.py
@@ -0,0 +1,10 @@
+#  Conjuntos / set 
+ conjunto = {1,2,3,4,5}
+otro_conjunto = {"hola", "como", "estas"}
+conjunto_vacio = set()
+
+ # son heterogeneos 
+ variable1 = "esto es una variable"
+datos = {1,-5,123.57, "una cadena", "otro string", variable1}
+print(datos)
+
